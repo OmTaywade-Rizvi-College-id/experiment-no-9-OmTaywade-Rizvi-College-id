e5af870791bebf97648edfb415256c29f0826496
Om Satish Taywade <omstaywade@eng.rizvi.edu.in>
2026-01-31 21:21:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Om Satish Taywade
+# Date: 29/01/2026
 
 print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+def Factorial(n):
+    if(n < 0):
+        print(f"Factorial of {-n} is Not Defined")
+    
+    elif(n == 0 or n == 1):
+        print(f"Factorial of {n} is 1")
+    
+    else:
+        fact= 1
+        for i in range(1, int(n+1)):
+            fact *= i
+        print(f"Factorial of {n} is {fact}")
+
+num = int(input("Enter Number: "))
+Factorial(num)
